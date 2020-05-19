2118ca4936c509b6eaf6ab8b46c254ca604a278b
Heitor Vieira <heitorvieira.pvd@gmail.com>
2020-05-19 04:05:47 -0300

    começando o app

--- todo/lib/main.dart
+++ todo/lib/main.dart
@@ -1,19 +1,13 @@
 import 'package:flutter/material.dart';
+import 'dart:io';
+import 'dart:async';
+import 'dart:convert';
+import 'package:path_provider/path_provider.dart';
 
 void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(),
-      home: Home(),
-    );
-  }
+  runApp(MaterialApp(
+    home: Home(),
+  ));
 }
 
 class Home extends StatefulWidget {
@@ -22,8 +16,54 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+
+  List _todoList = [];
+
+  Future<File> _getFile() async {
+    final directory = await getApplicationDocumentsDirectory();
+    return File("${directory.path}/data.json");
+  }//obter o arquivo
+
+  Future<File> _saveData() async {
+    String data = json.encode(_todoList);
+    final file = await _getFile();
+    return file.writeAsString(data);
+  }//salvar o arquivo
+
+  Future<String> _readData() async {
+    try{
+      final file = await _getFile();
+      return file.readAsString();
+    }catch (e) {
+      return null;
+    }
+  }//ler os dados do arquivo
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Lista de Tarefas"),
+        backgroundColor: Colors.blueAccent,
+        centerTitle: true
+      ) ,
+      body: Column(
+        children: <Widget>[
+          Container(
+            padding: EdgeInsets.fromLTRB(17.0, 1.0, 7.0, 1.0),
+            child: Row(
+              children: <Widget>[
+                TextField(
+                  decoration: InputDecoration(
+                    labelText: "Nova tarefa",
+                    labelStyle: TextStyle(color: Colors.blueAccent)
+                  ),
+
+                )
+            ],)
+          ),
+        ]
+      )
+    ) ;
   }
 }
