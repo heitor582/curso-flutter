1a25e2c233b9540fbe6130dfde7f0f6269148c54
Heitor Vieira <heitorvieira.pvd@gmail.com>
2020-07-06 12:27:59 -0300

    adding images and text in firebase

--- chat_on/lib/main.dart
+++ chat_on/lib/main.dart
@@ -1,9 +1,22 @@
 import 'package:flutter/material.dart';
-import 'package:chat_on/ui/homePage.dart';
+import 'package:chat_on/ui/chat_screen.dart';
 
 void main() {
-  runApp(MaterialApp(
-    title: 'Chat',
-    home: HomePage()
-  ));
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Chat',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        iconTheme: IconThemeData(
+          color: Colors.blue,
+        ),
+      ),
+      home: ChatScreen(),
+    );
+  }
 }
